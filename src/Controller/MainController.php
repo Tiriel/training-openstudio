72a7c3556d03f172e0de72b26beab40437dc9eef
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-16 12:13:14 +0100

    Exercise 2 : Request object

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,14 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_main_index', methods: ['GET'])]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return new Response('Hello World!');
+        $name = $request->query->get('name', 'World');
+
+        return new Response("Hello $name!");
+    }
+
+    #[Route('/contact', name: 'app_main_contact', methods: ['GET'])]
+    public function contact(): Response
+    {
+        return new Response('Contact');
     }
 }
