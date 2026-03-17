a01543e3571533dc33f95a3ad4c09d3d65d4ceca
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-17 16:45:02 +0100

    Exercise 24 : Contact form

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Dto\Contact;
+use App\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,9 +21,25 @@ class MainController extends AbstractController
         ]);
     }
 
-    #[Route('/contact', name: 'app_main_contact', methods: ['GET'])]
-    public function contact(): Response
+    #[Route('/contact', name: 'app_main_contact', methods: ['GET', 'POST'])]
+    public function contact(Request $request): Response
     {
-        return $this->render('main/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Contact $dto */
+            $dto = $form->getData();
+            $dto->setSentAt(new \DateTimeImmutable());
+
+            dump($dto);
+            $this->addFlash('info', 'Your message has been sent! Thank you '.$dto->getName());
+
+            return $this->redirectToRoute('app_main_contact');
+        }
+
+        return $this->render('main/contact.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
---
+++ src/Dto/Contact.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Dto;
+
+class Contact
+{
+    private ?\DateTimeImmutable $sentAt = null;
+
+    private ?string $name = null ;
+    private ?string $email = null ;
+    private ?string $subject = null ;
+    private ?string $message = null ;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): Contact
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): Contact
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): Contact
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message): Contact
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getSentAt(): ?\DateTimeImmutable
+    {
+        return $this->sentAt;
+    }
+
+    public function setSentAt(\DateTimeImmutable $sentAt): Contact
+    {
+        $this->sentAt = $sentAt;
+
+        return $this;
+    }
+}
