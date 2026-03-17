05e6d4a0318370e84e062f2c2e37933ef4565960
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-17 13:36:23 +0100

    Exercise 15 : List and Show json

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,16 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/conference')]
 class ConferenceController extends AbstractController
 {
+    #[Route('', name: 'app_conference_list', methods: ['GET'])]
+    public function list(ConferenceRepository $repository): Response
+    {
+        $conferences = \array_map(static fn(Conference $c) => [
+            'id' => $c->getId(),
+            'name' => $c->getName(),
+        ], $repository->findAll());
+
+        return $this->json($conferences);
+    }
+
+    #[Route('/{id}', name: 'app_conference_show', requirements: ['id' => Requirement::UUID], methods: ['GET'])]
+    public function show(Conference $conference): Response
+    {
+        return $this->json(['id' => $conference->getId(), 'name' => $conference->getName()]);
+    }
+
     #[Route(
-        '/conference/{name}/{start}/{end}',
+        '/{name}/{start}/{end}',
         name: 'app_conference_new',
         requirements: [
             'name' => Requirement::CATCH_ALL,
