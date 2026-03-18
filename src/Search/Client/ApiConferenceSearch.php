c6e75deb87572764b34c1f185a5bacb640301778
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-18 14:16:21 +0100

    Exercise 6 : Base client

--- src/Search/Client/ApiConferenceSearch.php
+++ src/Search/Client/ApiConferenceSearch.php
@@ -4,16 +4,24 @@ namespace App\Search\Client;
 
 use App\Search\Interface\ConferenceSearchInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiConferenceSearch implements ConferenceSearchInterface
 {
     public function __construct(
+        private readonly HttpClientInterface $client,
         #[Autowire(env: 'CONF_APIKEY')]
         private readonly string $apikey,
     ) {}
 
     public function searchByName(?string $name = null): array
     {
-        // TODO: Implement searchByName() method.
+        return $this->client->request('GET', 'https://www.devevents-api.fr/events', [
+            'query' => ['name' => $name],
+            'headers' => [
+                'apikey' => $this->apikey,
+                'Accept' => 'application/json',
+            ]
+        ])->toArray();
     }
 }
