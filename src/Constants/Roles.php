904f6bd43287bf8fb5f01c3923655ff08980f676
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-19 10:54:31 +0100

    Roles enum

---
+++ src/Constants/Roles.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+enum Roles
+{
+    public const USER = 'ROLE_USER';
+    public const WEBSITE = 'ROLE_WEBSITE';
+    public const VOLUNTEER = 'ROLE_VOLUNTEER';
+    public const ORGANIZER = 'ROLE_ORGANIZER';
+    public const ADMIN = 'ROLE_ADMIN';
+}
