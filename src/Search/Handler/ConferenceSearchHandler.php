e7abd3c162493432c767649e6315cbf442e28ff9
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2026-03-19 09:39:43 +0100

    Fix import

--- src/Search/Handler/ConferenceSearchHandler.php
+++ src/Search/Handler/ConferenceSearchHandler.php
@@ -2,6 +2,7 @@
 
 namespace App\Search\Handler;
 
+use App\Search\Database\DatabaseConferenceSearch;
 use App\Search\Interface\ConferenceSearchInterface;
 use Symfony\Component\DependencyInjection\Attribute\AsAlias;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
@@ -17,6 +18,14 @@ class ConferenceSearchHandler implements ConferenceSearchInterface
 
     public function searchByName(?string $name = null): array
     {
+        if (null === $name) {
+            foreach ($this->searches as $search) {
+                if ($search instanceof DatabaseConferenceSearch) {
+                    return $search->searchByName($name);
+                }
+            }
+        }
+        
         $conferences = [];
 
         foreach ($this->searches as $search) {
